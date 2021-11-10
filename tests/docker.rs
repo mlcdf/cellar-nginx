ad12173dbea54d73fe0d446b3286a8d90d6c43c9
Maxime Le Conte des Floris <git@mlcdf.fr>
2021-11-10 13:58:25 +0100

    add (failintg) index_html test

---
+++ tests/docker.rs
@@ -0,0 +1,88 @@
+use std::process::{Command, Output};
+
+pub struct RunOutput {
+    pub output: Output,
+    pub container_id: String,
+}
+
+pub fn build(tag: &str) -> Output {
+    let output = Command::new("docker")
+        .arg("build")
+        .arg(".")
+        .arg("-f")
+        .arg("tests/Dockerfile")
+        .arg("-t")
+        .arg(tag)
+        .output()
+        .expect("failed to execute process");
+
+    if !output.status.success() {
+        eprintln!(
+            "docker build stdout: {:}",
+            String::from_utf8_lossy(&output.stdout)
+        );
+        eprintln!(
+            "docker build stdout: {:}",
+            String::from_utf8_lossy(&output.stderr)
+        );
+    }
+    output
+}
+
+pub fn run(tag: &str) -> RunOutput {
+    let output = Command::new("docker")
+        .arg("run")
+        .arg("-p")
+        .arg("8080:8080")
+        .arg("-d")
+        .arg("-t")
+        .arg(tag)
+        .output()
+        .expect("failed to start container");
+
+    if !output.status.success() {
+        eprintln!(
+            "docker build stdout: {:}",
+            String::from_utf8_lossy(&output.stdout)
+        );
+        eprintln!(
+            "docker build stdout: {:}",
+            String::from_utf8_lossy(&output.stderr)
+        );
+    }
+
+    RunOutput {
+        container_id: String::from_utf8_lossy(&output.stdout).into_owned(),
+        output,
+    }
+}
+
+pub fn clean(id: &String) {
+    Command::new("sh")
+        .arg("-c")
+        .arg(format!("docker stop {}", id))
+        .output()
+        .expect("failed to stop container");
+
+    Command::new("sh")
+        .arg("-c")
+        .arg(format!("docker rm {}", id))
+        .output()
+        .expect("failed to remove container");
+}
+
+pub fn logs(id: &String) {
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(format!("docker logs {}", id))
+        .output()
+        .expect("failed to logs container");
+
+    if !output.status.success() {
+        eprintln!(
+            "docker logs stderr: {:}",
+            String::from_utf8_lossy(&output.stderr)
+        );
+    }
+    eprintln!("docker logs: {:}", String::from_utf8_lossy(&output.stdout));
+}
---
+++ tests/integration.rs
@@ -0,0 +1,40 @@
+use core::time;
+use std::thread;
+use ureq;
+
+mod docker;
+
+const IMAGE_TAG: &str = "nvhosts-test";
+
+#[test]
+fn syntax_check() {
+    let output = docker::build(IMAGE_TAG);
+    assert_eq!(output.status.code().unwrap(), 0)
+}
+
+#[test]
+fn index_html() {
+    docker::build(IMAGE_TAG);
+    let output = docker::run(IMAGE_TAG);
+
+    // wait for nginx to start
+    thread::sleep(time::Duration::from_secs(3));
+
+    let maybe_response = ureq::get("http://0.0.0.0:8080/index.html")
+        .set("Host", "dev.www.mlcdf.fr")
+        .call();
+
+    let response = match maybe_response {
+        Ok(response) => {
+            docker::clean(&output.container_id);
+            response
+        }
+        Err(err) => {
+            docker::logs(&output.container_id);
+            docker::clean(&output.container_id);
+            panic!("{}", err)
+        }
+    };
+
+    assert_eq!(response.status(), 200)
+}
