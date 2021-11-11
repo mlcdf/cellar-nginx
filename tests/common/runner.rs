e0bfc52df859ad7ac6897bd9777f44eddb90aa82
Maxime Le Conte des Floris <git@mlcdf.fr>
2021-11-11 18:09:43 +0100

    feat: re-enable www/no-www redirect

--- tests/common/runner.rs
+++ tests/common/runner.rs
@@ -1,5 +1,9 @@
 use core::time;
-use std::{env, panic, sync::Once, thread};
+use std::env;
+use std::net::TcpStream;
+use std::panic;
+use std::sync::Once;
+use std::thread;
 
 use super::docker;
 
@@ -9,12 +13,26 @@ fn setup() {
     docker::build(docker::IMAGE_TAG);
     let output = docker::run(docker::IMAGE_TAG);
 
+    let mut counter: u16 = 0;
+
     loop {
-        match ureq::get("http://0.0.0.0:8080/").call() {
+        match TcpStream::connect("0.0.0.0:8080") {
             Ok(_) => break,
-            Err(_) => {
-                eprintln!("\x1b[0;34mWaiting for container to be ready (10ms)\x1b[0m");
-                thread::sleep(time::Duration::from_millis(10))
+            Err(err) => {
+                if counter == 100 {
+                    docker::logs(&output.container_id);
+                    eprintln!("{:}", err);
+                    panic!(
+                        "\x1b[0;31mFailed to start container in less than {}ms\x1b[0m",
+                        10 * counter
+                    )
+                }
+                thread::sleep(time::Duration::from_millis(10));
+                counter += 1;
+                eprint!(
+                    "\x1b[0;34mWaiting for container to be ready ({}ms)\x1b[0m\x1B[0K\r",
+                    10 * counter
+                );
             }
         }
     }
